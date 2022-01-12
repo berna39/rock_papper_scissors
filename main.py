30079d79effc600a4e5ec0c12b5fcd6986668ed9
berna39 <kalemajoseph39@gmail.com>
2022-01-12 23:44:18 +0200

    Fixing bug

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from math import perm
 import random
 
+#main method
 def play():
     computer = random.choice(['r', 'p', 's'])
     player = input("Choose between 'r, 'p' and 's : \n")
 
     if player == compile:
         return 'it\'  a tie'
-    if is_win(player, computer):
+    if is_win(player, computer): #testing if th user wins
         return 'yay ! you  won'
     return 'you lost'
 
+#the is_win method
 def is_win(player, computer):
     if (player == 'r' and computer == 's') or ( player == 's' and computer == 'p' ) or (player == 'p' and computer =='r' ):
         return True
+    return False
  
 
 print(play())
